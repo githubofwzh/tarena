e53d9e0b8778f22ff8a9db3cdba21cf1612970bb
wzh <wswzh0329@sina.com>
2018-05-09 00:32:51 +0800

    add hello world 5

--- tarena/src/day01/HelloWorld.java
+++ tarena/src/day01/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("hello world");
 		System.out.println("hello world2");
 		System.out.println("hello world4");
+		System.out.println("hello world5");
 	}
 }
